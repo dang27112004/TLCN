174d3b8d0ba0e496677b0a6a991c28c373b27301
dang <nguyendang27112004@gmail.com>
2013-10-25 11:08:56 +0700

    face detector interface

---
+++ Desktop/FaceDetect/FaceDetect/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FaceDetect
+//
+//  Created by Dang on 10/22/13.
+//  Copyright (c) 2013 Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Desktop/FaceDetect/FaceDetect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FaceDetect
+//
+//  Created by Dang on 10/22/13.
+//  Copyright (c) 2013 Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
